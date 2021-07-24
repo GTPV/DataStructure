ed477daa901497cb61672b115ff3c350b88d2bde
GTPV <parangvo@gmail.com>
2021-07-24 15:31:29 +0900

    added size(), test passed

--- binary_search_tree/binary_search_tree.cpp
+++ binary_search_tree/binary_search_tree.cpp
@@ -26,12 +26,14 @@
 *
 *		variables:
 *				binary_search_tree_node* root => root node
-*				binary_search_tree_node* leaf => every last node has leaf node
+*				binary_search_tree_node* leaf => leaf node(parent, left_child, right_child : all nullptr)
+*				unsigned long long _size => size of bst(number of nodes in bst)
 *
 *		functions:
 *				insert => Insert value to bst
 *				remove => Delete value from bst. Return false if node doesn't exist.
 *				search => Search value node, return node pointer. Return nullptr if node doesn't exist.
+*				size => Return _size
 *				_transplant => link before's parent to after. nothing to do with both's children
 *				_minimum => return minimum-value-node in (temp_root-root subtree including temp_root)
 *				_maximum => return maximum-value-node in (temp_root-root subtree including temp_root)
@@ -57,8 +59,6 @@ struct binary_search_tree_node{
 };
 
 
-typedef binary_search_tree_node* bst_n_ptr;
-
 template<class T>
 bool _first_less_than_second(const T& _first, const T& _second){
 	return _first < _second;
@@ -69,42 +69,47 @@ template <class T>
 class binary_search_tree{
 public:
 	binary_search_tree();
-	bst_n_ptr search(const T& _value);
+	binary_search_tree(bool (*cmp)(T&, T&));
+	binary_search_tree_node<T>* search(const T& _value);
 	void insert(const T& _value);
 	bool remove(const T& _value);
+	unsigned long long size(void);
 private:
-	bst_n_ptr root;
-	bst_n_ptr leaf;
-	void _transplant(bst_n_ptr _before, bst_n_ptr _after);
+	binary_search_tree_node<T>* root;
+	binary_search_tree_node<T>* leaf;
+	unsigned long long _size;
+	void _transplant(binary_search_tree_node<T>* _before, binary_search_tree_node<T>* _after);
 	bool (*_comparison_function)(const T&, const T&);
-	bst_n_ptr _minimum_from_right(bst_n_ptr _temp_root);
-	bst_n_ptr _maximum_from_left(bst_n_ptr _temp_root);
+	binary_search_tree_node<T>* _minimum(binary_search_tree_node<T>* _temp_root);
+	binary_search_tree_node<T>* _maximum(binary_search_tree_node<T>* _temp_root);
 };
 
 
 template <class T>
 binary_search_tree<T>::binary_search_tree(){
-	leaf = new binary_search_tree_node;
-	leaf.parent = nullptr;
-	leaf.left_child = nullptr;
-	leaf.right_child = nullptr;
+	leaf = new binary_search_tree_node<T>;
+	leaf->parent = nullptr;
+	leaf->left_child = nullptr;
+	leaf->right_child = nullptr;
 	root = leaf;
+	_size = 0;
 	_comparison_function = _first_less_than_second;
 }
 
 template <class T>
 binary_search_tree<T>::binary_search_tree(bool (*cmp)(T&, T&)){
-	leaf = new binary_search_tree_node;
-	leaf.parent = nullptr;
-	leaf.left_child = nullptr;
-	leaf.right_child = nullptr;
+	leaf = new binary_search_tree_node<T>;
+	leaf->parent = nullptr;
+	leaf->left_child = nullptr;
+	leaf->right_child = nullptr;
 	root = leaf;
+	_size = 0;
 	_comparison_function = cmp;
 }
 
 template <class T>
-bst_n_ptr binary_search_tree<T>::search(const T& _value){
-	bst_n_ptr _node_to_check = root;
+binary_search_tree_node<T>* binary_search_tree<T>::search(const T& _value){
+	binary_search_tree_node<T>* _node_to_check = root;
 
 	while(_node_to_check != nullptr && _node_to_check->value != _value){
 		if(_comparison_function(_value, _node_to_check->value)){
@@ -114,30 +119,31 @@ bst_n_ptr binary_search_tree<T>::search(const T& _value){
 		}
 	}
 
-	return node_to_check;
+	return _node_to_check;
 }
 
 template <class T>
 void binary_search_tree<T>::insert(const T& _value){
-	bst_n_ptr _node_to_insert = new binary_search_tree_node();
+	binary_search_tree_node<T>* _node_to_insert = new binary_search_tree_node<T>;
 	_node_to_insert->value = _value;
 	_node_to_insert->parent = nullptr;
 	_node_to_insert->left_child = leaf;
 	_node_to_insert->right_child = leaf;
 
-	if(search(value) != nullptr) return;
+	if(search(_value) != nullptr) return;
 
-	bst_n_ptr _where_to_insert = this->root;
+	binary_search_tree_node<T>* _where_to_insert = this->root, *_where_to_insert_parent = nullptr;
 
 	while(_where_to_insert != leaf){
-		if(_comparison_function(value, _where_to_insert->value)){
+		_where_to_insert_parent = _where_to_insert;
+		if(_comparison_function(_value, _where_to_insert->value)){
 			_where_to_insert = _where_to_insert->left_child;
 		} else{
 			_where_to_insert = _where_to_insert->right_child;
 		}
 	}
 
-	_node_to_insert->parent = _where_to_insert->parent;
+	_node_to_insert->parent = _where_to_insert_parent;
 
 	if(_node_to_insert->parent == nullptr){
 		root = _node_to_insert;
@@ -146,43 +152,51 @@ void binary_search_tree<T>::insert(const T& _value){
 	} else{
 		_node_to_insert->parent->left_child = _node_to_insert;
 	}
+
+	_size += 1;
 }
 
 template <class T>
-bool binary_search_tree<T>::remove(const T& value){
-	bst_n_ptr _node_to_remove = search(value);
+bool binary_search_tree<T>::remove(const T& _value){
+	binary_search_tree_node<T>* _node_to_remove = search(_value);
 
 	if(_node_to_remove == nullptr){
 		return false;
 	} else{
-		bst_n_ptr _node_to_replace;
+		binary_search_tree_node<T>* _node_to_replace;
 
 		if(_node_to_remove->left_child == leaf){
-			transplant(_node_to_remove, _node_to_remove->right_child);
+			_transplant(_node_to_remove, _node_to_remove->right_child);
 		} else if(_node_to_remove->right_child == leaf){
-			transplant(_node_to_remove, _node_to_remove->left_child);
+			_transplant(_node_to_remove, _node_to_remove->left_child);
 		} else{
 			_node_to_replace = _minimum(_node_to_remove->right_child);
 			if(_node_to_replace->parent != _node_to_remove){
-				transplant(_node_to_replace, _node_to_replace->right_child);
+				_transplant(_node_to_replace, _node_to_replace->right_child);
 				_node_to_replace->right_child = _node_to_remove->right_child;
-				transplant(_node_to_remove, _node_to_replace);
+				_transplant(_node_to_remove, _node_to_replace);
 				_node_to_replace->left_child = _node_to_remove->left_child;
 				_node_to_replace->left_child->parent = _node_to_replace;
 				_node_to_replace->right_child->parent = _node_to_replace;
 			} else{
-				transplant(_node_to_remove, _node_to_replace);
+				_transplant(_node_to_remove, _node_to_replace);
 				_node_to_replace->left_child = _node_to_remove->left_child;
 				_node_to_remove->left_child->parent = _node_to_replace;
 			}
 		}
 		delete _node_to_remove;
+		_size -= 1;
 		return true;
 	}
 }
 
 template <class T>
-void binary_search_tree<T>::_transplant(bst_n_ptr _before, bst_n_ptr _after){
+unsigned long long binary_search_tree<T>::size(void){
+	return _size;
+}
+
+template <class T>
+void binary_search_tree<T>::_transplant(binary_search_tree_node<T>* _before, binary_search_tree_node<T>* _after){
 	if(_before->parent == nullptr){
 		root = _after;
 	} else if(_before == _before->parent->left_child){
@@ -195,7 +209,7 @@ void binary_search_tree<T>::_transplant(bst_n_ptr _before, bst_n_ptr _after){
 }
 
 template <class T>
-bst_n_ptr binary_search_tree<T>::_minimum(bst_n_ptr _temp_root){
+binary_search_tree_node<T>* binary_search_tree<T>::_minimum(binary_search_tree_node<T>* _temp_root){
 	while(_temp_root->left_child != leaf){
 		_temp_root = _temp_root->left_child;
 	}
@@ -203,7 +217,7 @@ bst_n_ptr binary_search_tree<T>::_minimum(bst_n_ptr _temp_root){
 }
 
 template <class T>
-bst_n_ptr binary_search_tree<T>::_maximum(bst_n_ptr _temp_root){
+binary_search_tree_node<T>* binary_search_tree<T>::_maximum(binary_search_tree_node<T>* _temp_root){
 	while(_temp_root->right_child != leaf){
 		_temp_root = _temp_root->right_child;
 	}
