3c98b4ebdb5ad9afd85bc3f65a1702f6f98ef8e1
GTPV <parangvo@gmail.com>
2022-07-06 17:05:26 +0900

    documentation added

--- red_black_tree/red_black_tree.cpp
+++ red_black_tree/red_black_tree.cpp
@@ -1,3 +1,59 @@
+/*
+* Implementation of red_black_tree
+*
+*
+* red_black_tree implementation by double linked list
+* generic programming by template
+*
+*
+***************************************************************************************************
+*       class red_black_tree_node
+*
+*               variables:
+*                           value => value of the node
+*                           color => color of node by enum : red or black
+*                           parent => parent node pointer
+*                           left_child => left child node pointer
+*                           right_child => right child node pointer
+*
+*
+***************************************************************************************************
+*       class red_black_tree
+*
+*               consturctor:
+*                           make new node : leaf(=root) -> set parent && children to nullptr
+*
+*
+*               variables:
+*
+*                   public:
+*                           no public variable
+*
+*                   private:
+*                           red_black_tree_node* root => root node
+*                           red_black_tree_node* leaf => leaf node : parent==left_child==right_child==nullptr
+*                           unsigned long long _size => size of red_black_tree(number of nodes)
+*
+*
+*               functions:
+*
+*                   public:
+*                           search => Return node pointer whose value is _value. If node with _value doesn't exist, return nullptr.
+*                           insert => Insert node with _value to RBtree.
+*                           remove => Remove node whose value is _value. If successfully removed a node, return true. Otherwise if node with _value doesn't exist, return false.
+*                           size => Return _size.
+*
+*
+*
+***************************************************************************************************
+
+*
+*
+*
+*
+*
+*
+*/
 #pragma once
 #include <functional>
 
@@ -63,6 +119,15 @@ red_black_tree<T>::red_black_tree(std::function<bool(const T&, const T&)> cmp){
 
 template <class T>
 red_black_tree_node<T>* red_black_tree<T>::search(const T& _value){
+    red_black_tree_node<T>* _node_to_return = root;
+
+    while(_node_to_return != nullptr && _node_to_return->value != _value){
+        if(_comparison_function(_value, _node_to_return->value)){
+            _node_to_return = _node_to_return->left_child;
+        } else{
+            _node_to_return = _node_to_return->right_child;
+        }
+    }
 }
 
 template <class T>
